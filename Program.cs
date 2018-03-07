c1765d758735343ee4a144057aba54ef7cf32f7f
diana1190 <dagutierrez1190@gmail.com>
2018-03-07 12:36:28 -0600

    inicial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Escriba su carrera");
+            string carrera = Console.ReadLine();
+            if (carrera == "sistemas")
+            {
+                Console.WriteLine("Estás en la mejor carrera del mundo");
+            }
+            else
+            {
+                Console.WriteLine("¡No puedes con el reto!");
+            }
+        }
+    }
+}
